624dbb8f8c40133758df46130ce023416dbaebda
github_Venessa <vasiedu010@st.ug.edu.gh>
2024-05-18 19:09:45 -0700

    ProcessArray

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,20 @@
-function
+function processArray(numbers) {
+    let result = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+
+
+        if (number % 2 === 0) {
+            result.push(number * number);
+        } else {
+            result.push(number * 3);
+        }
+    }
+
+    return result;
+}
+
+let inputArray = [34, 2, 9, 4, 8];
+let processedArray = processArray(inputArray);
+console.log(processedArray);
